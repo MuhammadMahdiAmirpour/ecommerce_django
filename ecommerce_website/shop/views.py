ac0ff336074e820a364f591ed5ccb540efcdf267
MuhammadMahdiAmirpour <muhammadmahdiamirpour@gmail.com>
2023-09-01 15:56:11 +0330

    implemented search functionality

--- ecommerce_website/shop/views.py
+++ ecommerce_website/shop/views.py
@@ -5,5 +5,8 @@ from .models import Product
 
 def index(request):
     product_objects = Product.objects.all()
+    item_name = request.GET.get('item_name')
+    if all([item_name!='', item_name is not None]):
+        product_objects = product_objects.filter(title__icontains = item_name)
     return render(request, 'shop/index.html', {'product_objects': product_objects})
 
