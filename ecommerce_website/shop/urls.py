7d36afda15b584ffabf975e735fd4119dfd6515d
MuhammadMahdiAmirpour <muhammadmahdiamirpour@gmail.com>
2023-09-02 16:34:29 +0330

    detail view implemented

--- ecommerce_website/shop/urls.py
+++ ecommerce_website/shop/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from .views import index
+from .views import detail
 
 urlpatterns = [
-        path('index/', index, name='index')
+        path('index/', index, name='index'),
+        path('detail/<int:id>', detail, name='detail'),
 ]
